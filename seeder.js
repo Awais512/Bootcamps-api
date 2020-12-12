d7df0a94d81269fee05ef855e71224ea37a9ef8a
awais512 <raza.awais25@gmail.com>
2020-12-12 21:04:37 +0500

    Course Model and Seed courses into db

--- seeder.js
+++ seeder.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/bootcampModel');
+const Course = require('./models/courseModel');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -18,10 +19,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 //Import into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -33,6 +39,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
   } catch (err) {
