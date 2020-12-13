dcb21b0d7133bec7803a34b056fe9fe91bc53a7f
awais512 <raza.awais25@gmail.com>
2020-12-13 23:01:47 +0500

    Send jwt in cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,9 +9,7 @@ exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
   const user = await User.create({ name, email, password, role });
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 //@desc     Login User and get token
@@ -36,7 +34,26 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
+  sendTokenResponse(user, 200, res);
+});
+
+//Get token from model, Create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie('token', token, options).json({
+    success: true,
+    token,
+  });
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const colors = require('colors');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env' });
 const connectDb = require('./config/db');
@@ -18,6 +19,7 @@ const authRoutes = require('./routes/authController');
 connectDb();
 
 app.use(express.json());
+app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
