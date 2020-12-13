2df953e6699a1c441cb63333a16ad7bd1df9cc8c
awais512 <raza.awais25@gmail.com>
2020-12-13 22:06:02 +0500

    Get Signed jwt token on registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,8 +9,9 @@ exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
   const user = await User.create({ name, email, password, role });
+  const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true });
+  res.status(200).json({ success: true, token });
 });
 
 //@desc     Login User and get token
