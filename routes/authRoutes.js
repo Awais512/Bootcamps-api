84cde3dc7573007e5d380a0797f3186900434d76
awais512 <raza.awais25@gmail.com>
2020-12-16 14:50:46 +0500

    Logout to clear Token cookie

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,12 +7,14 @@ const {
   updateDetails,
   me,
   updatePassword,
+  logout,
 } = require('../controllers/authController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 router.put('/updatedetails', protect, updateDetails);
